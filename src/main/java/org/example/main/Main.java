8a5e90a85fa06b6dd92e456687757b511d097470
penzata <penzata86@yahoo.com>
2022-11-21 01:14:16 +0200

    commit - adding logger

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,7 +1,16 @@
 package org.example.main;
 
-public class Main {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Main {
+    static Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        log.trace("tracing");
+        log.debug("debugging");
+        log.info("info");
+        log.warn("warning");
+        log.error("error");
+        log.info("info2");
     }
 }

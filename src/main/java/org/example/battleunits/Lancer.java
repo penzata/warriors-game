118cb39a0b68f203e646f39ebe02129aa3f73f6b
penzata <penzata86@yahoo.com>
2022-11-20 23:08:42 +0200

    commit - new class Lancer

---
+++ src/main/java/org/example/battleunits/Lancer.java
@@ -0,0 +1,26 @@
+package org.example.battleunits;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Lancer extends Warrior {
+
+    /**
+     * Constructs default Lancer object with default health(50) & attack(6).
+     */
+    public Lancer() {
+
+        super(50, 6);
+    }
+
+    public Lancer(int health, int attack) {
+
+        super(health, attack);
+    }
+
+    public Lancer(@NotNull Lancer lancer) {
+
+        super(lancer);
+    }
+}
+
+

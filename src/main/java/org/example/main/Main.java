eed9523868521378ffed6b9a426d9a3cf84e2597
penzata <penzata86@yahoo.com>
2022-11-21 02:00:02 +0200

    commit - logging

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
     static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         log.trace("tracing");
         log.debug("debugging");
         log.info("info");
         log.warn("warning");
         log.error("error");
-        log.info("info2");
+
+        log.info("Logger name: {}", log.getName());
     }
 }

a4068cb415360da6e7f1af521ed159675cd256f3
penzata <penzata86@yahoo.com>
2022-11-22 13:58:25 +0200

    commit - refactoring

--- src/main/java/org/example/fighting/Duel.java
+++ src/main/java/org/example/fighting/Duel.java
@@ -1,8 +1,11 @@
 package org.example.fighting;
 
 import org.example.battleunits.units.WarriorUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Duel {
+    static Logger log = LoggerFactory.getLogger(Duel.class);
 
     private Duel() {
     }
@@ -15,8 +18,11 @@ public class Duel {
     public static boolean fight(WarriorUnit attacker, WarriorUnit defender) {
         while (attacker.isAlive() && defender.isAlive()) {
             attacker.hit(defender);
+            log.info("Vampire health after getting hit: {}", defender.getHealth());
             if (defender.isAlive()) {
                 defender.hit(attacker);
+                log.info("Defender health after getting hit: {}", attacker.getHealth());
+                log.info("vampire health after vampirism: {}", defender.getHealth());
             }
         }
         return attacker.isAlive();
--- src/test/java/org/example/battleunits/LancerTest.java
+++ src/test/java/org/example/battleunits/LancerTest.java
@@ -1,28 +1,40 @@
 package org.example.battleunits;
 
 import org.example.battleunits.units.ArmyUnit;
-import org.example.battleunits.units.KnightUnit;
 import org.example.battleunits.units.LancerUnit;
 import org.example.battleunits.units.WarriorUnit;
 import org.example.fighting.Battle;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LancerTest {
 
     @Test
     void LancerFightsArmyOfTwoAndMakesPiercingDamage() {
-        ArmyUnit lancer = new Army(LancerUnit::newLancer, 1);
-        ArmyUnit rookiesArmy = new Army(WarriorUnit::newWarrior, 1)
-                .addBattleUnits(KnightUnit::newKnight, 1);
+        ArmyUnit oneLancerArmy = new Army(LancerUnit::newLancer, 1);
+        ArmyUnit army = new Army(WarriorUnit::newWarrior, 1)
+                .addBattleUnits(() -> new Knight(25, 7), 1);
 
+        assertTrue(Battle.fight(oneLancerArmy, army));
+    }
 
-        assertFalse(Battle.fight(lancer, rookiesArmy));
+    @Test
+    void LancerFightsTwoRookieArmy_AlwaysSetRookieHealthToOne_BreakProgramTest() {
+        ArmyUnit lancerArmy = new Army(LancerUnit::newLancer, 1);
+        ArmyUnit rookiesArmy = new Army(Rookie::new, 1)
+                .addBattleUnits(Rookie::new, 1);
 
+        assertFalse(Battle.fight(lancerArmy, rookiesArmy));
     }
 
     class Rookie extends Warrior {
+
+        @Override
+        public int getHealth() {
+            return 1;
+        }
 
         @Override
         public int getAttack() {

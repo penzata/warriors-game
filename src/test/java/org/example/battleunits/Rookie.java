7800e097f1cd4bd138cd238846b3e189f19911b3
penzata <penzata86@yahoo.com>
2022-12-02 14:36:46 +0200

    commit - refactoring

--- src/test/java/org/example/battleunits/Rookie.java
+++ src/test/java/org/example/battleunits/Rookie.java
@@ -3,7 +3,9 @@ package org.example.battleunits;
 
 import org.example.battleunits.weapons.Weapon;
 import org.example.battleunits.weapons.WeaponType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,16 +16,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class Rookie extends WarriorImpl {
+    Rookie rookie;
 
     public Rookie() {
         super(50, 1);
+    }
+
+    @BeforeEach
+    void init() {
+        this.rookie = new Rookie();
     }
 
     @DisplayName("different weapons equipped by Rookie")
     @ParameterizedTest(name = "equipped {0}")
     @MethodSource({"equipWeapon"})
     void EquipDifferentWeaponsOnRookieAndVerifyItsStats(Weapon weapon, int expectedHealth, int expectedAttack) {
-        Rookie rookie = new Rookie();
         rookie.equipWeapon(weapon);
 
         assertEquals(expectedHealth, rookie.getHealth());
@@ -39,6 +46,18 @@ public class Rookie extends WarriorImpl {
                 arguments(WeaponType.MAGIC_WAND, 80, 4),
                 arguments(Weapon.builder().healthStat(-100).attackStat(-100).defenceStat(-100)
                         .vampirismStat(-100).healPowerStat(-100).build(), -50, 0, 0));
+    }
+
+    @Test
+    void RookieDiesFromWeaponOverEquippment() {
+        rookie.equipWeapon(WeaponType.SWORD)
+                .equipWeapon(WeaponType.KATANA)
+                .equipWeapon(WeaponType.GREAT_AXE)
+                .equipWeapon(WeaponType.GREAT_AXE)
+                .equipWeapon(WeaponType.KATANA)
+                .equipWeapon(WeaponType.MAGIC_WAND);
+
+        assertEquals(-15, rookie.getHealth());
     }
 
 }
